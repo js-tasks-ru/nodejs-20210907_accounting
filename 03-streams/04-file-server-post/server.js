cfe801f57394fb009b13ea38e42e70a0f668c525
Anna Ostapenko <kvintabudinvest@gmail.com>
2021-09-19 14:46:05 +0200

    added fixes to task 3

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -12,47 +12,48 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (req.url.split('/').length > 2) {
+    res.statusCode = 400;
+    res.end('Nested folders are not supported')
+    return;
+  }
   switch (req.method) {
     case 'POST':
-      if (req.url.split('/').length > 2) {
-        res.statusCode = 400;
-        res.end('Nested folders are not supported')
-      }
-      else if (fs.existsSync(filepath)) {
-        res.statusCode = 409
-        res.end('File exists')
-      } else {
-        const stream = fs.createWriteStream(filepath)
-        stream.on('error', () => {
+      const stream = fs.createWriteStream(filepath, { flags: 'wx' })
+      const limitSizeStream = new LimitSizeStream({ limit: Math.pow(2, 20) })
+
+      req.pipe(limitSizeStream).pipe(stream);
+
+      stream.on('error', (err) => {
+        if (err.code === 'EEXIST') {
+          res.statusCode = 409
+          res.end('File exists')
+        } else {
           res.statusCode = 500
           res.end('Server error')
-        })
-        const limitSizeStream = new LimitSizeStream({ limit: Math.pow(2, 20) })
+        }
+      })
 
-        req.pipe(limitSizeStream).pipe(stream);
-
-        limitSizeStream.on('error', (err) => {
-          if (err.code === 'LIMIT_EXCEEDED') {
-            res.statusCode = 413
-            res.end('Limit exceeded')
-            fs.unlink(filepath, (err) => { })
-          } else {
-            res.statusCode = 500
-            res.end('Server error')
-          }
-        })
-        req.on('aborted', () => {
-          stream.destroy();
-          limitSizeStream.destroy()
+      limitSizeStream.on('error', (err) => {
+        if (err.code === 'LIMIT_EXCEEDED') {
+          res.statusCode = 413
+          res.end('Limit exceeded')
           fs.unlink(filepath, (err) => { })
-        })
+        } else {
+          res.statusCode = 500
+          res.end('Server error')
+        }
+      })
+      req.on('aborted', () => {
+        stream.destroy();
+        limitSizeStream.destroy()
+        fs.unlink(filepath, (err) => { })
+      })
 
-        req.on('end', () => {
-          res.statusCode = 200
-          res.end('Filed saved')
-        })
-      }
-
+      stream.on('finish', () => {
+        res.statusCode = 201
+        res.end('Filed saved')
+      })
       break;
 
     default:
